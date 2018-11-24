2685660b4456c39a3986abbb06bce3ff92361ff3
SuperBuddy <36763574+SuperBuddys@users.noreply.github.com>
2018-11-25 00:51:24 +0530

    shared : Layout(M)-NavBar(C)-Footer(C)

---
+++ softonis/src/app/shared/layout/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NavBarComponent, FooterComponent]
+})
+export class LayoutModule { }
