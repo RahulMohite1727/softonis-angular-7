4b1c938747f12457098021b3fcd251001021bc8d
pravinpokharkar <pravin.a.pokharkar@gmail.com>
2018-11-28 10:57:04 +0530

    Softonis:  Updated Project Arch

--- softonis/src/app/app.module.ts
+++ softonis/src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { HomeModulesModule } from './home-modules/home-modules.module';
-import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-
+import { AppComponent } from './app.component';
+import { NavbarModule } from './navbar/navbar.module';
+import { FooterModule } from './footer/footer.module';
+import { FeaturesModule } from './features/features.module';
+import { HomeModule } from './home/home.module';
+import { ServicesModule } from './services/services.module';
+import { AboutModule } from './about/about.module';
+import { BlogModule } from './blog/blog.module';
+import { CareersModule } from './careers/careers.module';
+import { PricingModule } from './pricing/pricing.module';
+import { ContactModule } from './contact/contact.module';
+import { TeamModule } from './team/team.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule,
-    SharedModule,
-    HomeModulesModule
+    NavbarModule,
+    FooterModule,
+    ServicesModule,
+    AboutModule,
+    BlogModule,
+    CareersModule,
+    ContactModule,
+    PricingModule,
+    TeamModule,
+    HomeModule,
+    FeaturesModule
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ softonis/src/app/careers/careers.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CareersComponent } from './careers.component';
+
+@NgModule({
+  declarations: [CareersComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CareersModule { }
