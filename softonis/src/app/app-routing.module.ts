4f5ef7f6d99785693c614e0df354109f4a1b7996
SonalMane254 <manesonal254@gmail.com>
2018-12-03 12:34:07 -0500

    order :  complete module

--- softonis/src/app/app-routing.module.ts
+++ softonis/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NewsComponent } from './about/news/news.component';
+import { CareersComponent } from './careers/careers.component';
+import { AboutComponent } from './about/about.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact.component';
 
-const routes: Routes = [
+const routes: Routes = [{
+  path : 'about', component : AboutComponent
+},
+{
+  path : 'news', component : NewsComponent
+},
+{ path : 'careers', component : CareersComponent
+},
+{
+  path : 'contact', component : ContactComponent
+}
   
 ];
 
