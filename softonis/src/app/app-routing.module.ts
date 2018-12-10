de7b486bda046317d3b45b751abd570add9389fd
rahulmore942 <rahulmore942@gmail.com>
2018-12-10 11:05:04 +0530

    need to pull all data

--- softonis/src/app/app-routing.module.ts
+++ softonis/src/app/app-routing.module.ts
@@ -1,51 +1,34 @@
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContactCrdComponent } from './contact/contact-crd/contact-crd.component';
 import { ContactFormComponent } from './contact/contact-form/contact-form.component';
 import { ContactInfoComponent } from './contact/contact-info/contact-info.component';
 import { ContactMapComponent } from './contact/contact-map/contact-map.component';
-import { NewsComponent } from './about/news/news.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> aa93f7cabe7d6e75ac189c06abd1a18262be9abe
+import { NewsComponent } from './about/news/news.component';
 import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { PricingComponent } from './pricing/pricing.component';
-const routes: Routes = [
+import { Routes, RouterModule } from '@angular/router';
 
-      
+
+
+
+
+const routes: Routes = [
  {path:'home',component:HomeComponent},
- {path:'pricing',component:PricingComponent}
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-// const ContactComponent = [
-//   {path : 'contact', component : ContactCrdComponent},
-//   {path : 'contact', component : ContactInfoComponent},
-//   {path : 'contact', component : ContactFormComponent},
-//   {path : 'contact', component : ContactMapComponent},
-// ];
-
-
-const routes: Routes = [
-  {path : 'about', component : AboutComponent},
+ {path:'pricing',component:PricingComponent},
+ {path : 'about', component : AboutComponent},
   {path : 'news', component : NewsComponent},
   {path : 'contact', component : ContactComponent}
-=======
-   
->>>>>>> aa93f7cabe7d6e75ac189c06abd1a18262be9abe
-  
 ];
+
+
+// const routes: Routes = [
+//   {path : 'about', component : AboutComponent},
+//   {path : 'news', component : NewsComponent},
+//   {path : 'contact', component : ContactComponent}
+  
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- softonis/src/app/app.module.ts
+++ softonis/src/app/app.module.ts
@@ -1,19 +1,19 @@
 
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarModule } from './navbar/navbar.module';
-import { FooterModule } from './footer/footer.module';
-import { FeaturesModule } from './features/features.module';
-import { HomeModule } from './home/home.module';
-import { ServicesModule } from './services/services.module';
 import { AboutModule } from './about/about.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BlogModule } from './blog/blog.module';
 import { CareersModule } from './careers/careers.module';
-import { PricingModule } from './pricing/pricing.module';
 import { ContactModule } from './contact/contact.module';
+import { FeaturesModule } from './features/features.module';
+import { FooterModule } from './footer/footer.module';
+import { HomeModule } from './home/home.module';
+import { NavbarModule } from './navbar/navbar.module';
+import { NgModule } from '@angular/core';
+import { PricingModule } from './pricing/pricing.module';
+import { ServicesModule } from './services/services.module';
 import { TeamModule } from './team/team.module';
+
 
 @NgModule({
   declarations: [
